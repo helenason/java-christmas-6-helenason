bd70a999fdf0297c81db4b29b487c446b031974b
SCY <helenason@naver.com>
2023-11-10 19:37:31 +0900

    feat: 사용자로부터 예상 방문 날짜 입력받기

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,13 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String ASK_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+
+    public String readVisitDate() {
+        System.out.println(ASK_VISIT_DATE);
+        return Console.readLine();
+    }
+}
