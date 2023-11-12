1f47b90af20b3297aa4c03b8bda91e710ae9e874
SCY <helenason@naver.com>
2023-11-13 02:16:47 +0900

    feat: 평일 또는 주말 관련 할인 금액 계산하기

--- src/main/java/christmas/model/event/DayDiscount.java
+++ src/main/java/christmas/model/event/DayDiscount.java
@@ -1,5 +1,43 @@
 package christmas.model.event;
 
+import christmas.constant.Menu;
+
+import java.util.Map;
+import java.util.Set;
+
 public class DayDiscount extends Event {
+
+    public int calculate(Map<Menu, Integer> orderMenu, int date) {
+        if (calendar.isWeekend(date)) {
+            // 메인 메뉴 1개당 2023 할인 (종류 한 개 ??)
+            return discountMainMenu(orderMenu);
+        }
+        // 디저트 메뉴 1개당 2023 할인
+        return discountDessertMenu(orderMenu);
+    }
+
+    private int discountDessertMenu(Map<Menu, Integer> orderMenu) {
+        int discountAmount = 0;
+        Set<Menu> menus = orderMenu.keySet();
+        for (Menu menu : menus) {
+            if (menu.isDessertType()) {
+                int count = orderMenu.get(menu);
+                discountAmount += 2023 * count;
+            }
+        }
+        return discountAmount;
+    }
+
+    private int discountMainMenu(Map<Menu, Integer> orderMenu) {
+        int discountAmount = 0;
+        Set<Menu> menus = orderMenu.keySet();
+        for (Menu menu : menus) {
+            if (menu.isMainType()) {
+                int count = orderMenu.get(menu);
+                discountAmount += 2023 * count;
+            }
+        }
+        return discountAmount;
+    }
 
 }
