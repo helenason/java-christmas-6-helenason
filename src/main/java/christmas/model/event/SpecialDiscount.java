93fefb1ef02eb5b87f8a596a24d5f18e12dcf772
SCY <helenason@naver.com>
2023-11-13 02:19:31 +0900

    feat: 특별 할인 시 할인 금액 계산하기

--- src/main/java/christmas/model/event/SpecialDiscount.java
+++ src/main/java/christmas/model/event/SpecialDiscount.java
@@ -1,4 +1,12 @@
 package christmas.model.event;
 
 public class SpecialDiscount extends Event {
+
+    public int calculate(int date) {
+        if (calendar.isSpecialDay(date)) {
+            return 1000;
+        }
+        return 0;
+    }
+
 }
