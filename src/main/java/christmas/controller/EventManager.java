cf9137e492faa6ddbb104da68493a1b687bb1a9c
SCY <helenason@naver.com>
2023-11-14 01:23:31 +0900

    refactor: 새로운 controller 생성하여 책임 분배하기

---
+++ src/main/java/christmas/controller/EventManager.java
@@ -0,0 +1,75 @@
+package christmas.controller;
+
+import christmas.model.Order;
+import christmas.model.event.ChristmasDiscount;
+import christmas.model.event.DayDiscount;
+import christmas.model.event.PresentEvent;
+import christmas.model.event.SpecialDiscount;
+import christmas.view.OutputView;
+
+public class EventManager {
+
+    private static final int EVENT_START_DATE = 1;
+    private static final int EVENT_END_DATE = 31;
+    private static final int EVENT_CHRISTMAS_END_DATE = 25;
+
+    private final OutputView outputView;
+    private final ChristmasDiscount christmasDiscount;
+    private final DayDiscount dayDiscount;
+    private final SpecialDiscount specialDiscount;
+    private final PresentEvent presentEvent;
+
+    public EventManager(OutputView outputView) {
+        this.outputView = outputView;
+        christmasDiscount = new ChristmasDiscount(EVENT_START_DATE, EVENT_CHRISTMAS_END_DATE);
+        dayDiscount = new DayDiscount(EVENT_START_DATE, EVENT_END_DATE);
+        presentEvent = new PresentEvent(EVENT_START_DATE, EVENT_END_DATE);
+        specialDiscount = new SpecialDiscount(EVENT_START_DATE, EVENT_END_DATE);
+    }
+
+    public void organizeBenefits(Order order, int date) {
+        outputView.printDiscountDetails();
+
+        int christmasDiscountAmount = organizeChristmasBenefits(date);
+        int dayDiscountAmount = organizeDayBenefits(order, date);
+        int specialDiscountAmount = organizeSpecialBenefits(date);
+        int presentEventAmount = organizePresentBenefits(order);
+        outputView.printNewLine();
+
+        int totalDiscountAmount = christmasDiscountAmount
+                + dayDiscountAmount
+                + specialDiscountAmount
+                + presentEventAmount;
+        outputView.printTotalDiscount(totalDiscountAmount);
+        outputView.printNewLine();
+
+        int totalAmount = order.getTotalAmount();
+        int expectedAmount = totalAmount - totalDiscountAmount + presentEventAmount;
+        outputView.printExpectedAmount(expectedAmount);
+        outputView.printNewLine();
+    }
+
+    private int organizeChristmasBenefits(int date) {
+        int christmasDiscountAmount = christmasDiscount.calculate(date);
+        outputView.printChristmasDiscount(christmasDiscountAmount);
+        return christmasDiscountAmount;
+    }
+
+    private int organizeDayBenefits(Order order, int date) {
+        int dayDiscountAmount = dayDiscount.calculate(order, date);
+        outputView.printDayDiscount(dayDiscountAmount);
+        return dayDiscountAmount;
+    }
+
+    private int organizeSpecialBenefits(int date) {
+        int specialDiscountAmount = specialDiscount.calculate(date);
+        outputView.printSpecialDiscount(specialDiscountAmount);
+        return specialDiscountAmount;
+    }
+
+    private int organizePresentBenefits(Order order) {
+        int presentEventAmount = presentEvent.calculate(order.getTotalAmount());
+        outputView.printPresentEvent(presentEventAmount);
+        return presentEventAmount;
+    }
+}
--- src/main/java/christmas/controller/Planner.java
+++ src/main/java/christmas/controller/Planner.java
@@ -4,10 +4,6 @@ import christmas.constant.Menu;
 import christmas.model.DecemberCalendar;
 import christmas.model.Order;
 import christmas.model.VisitDate;
-import christmas.model.event.ChristmasDiscount;
-import christmas.model.event.DayDiscount;
-import christmas.model.event.PresentEvent;
-import christmas.model.event.SpecialDiscount;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
@@ -15,30 +11,18 @@ import java.util.Set;
 
 public class Planner {
 
-    private static final int EVENT_START_DATE = 1;
-    private static final int EVENT_END_DATE = 1;
-    private static final int EVENT_CHRISTMAS_END_DATE = 1;
-
-
     private final InputView inputView;
     private final OutputView outputView;
+    private final EventManager eventManager;
     private VisitDate visitDate;
     private Order order;
-
-    private ChristmasDiscount christmasDiscount;
-    private DayDiscount dayDiscount;
-    private PresentEvent presentEvent;
-    private SpecialDiscount specialDiscount;
 
     public Planner() {
         inputView = new InputView();
         outputView = new OutputView();
+        eventManager = new EventManager(outputView);
         init();
         DecemberCalendar.of();
-        christmasDiscount = new ChristmasDiscount(EVENT_START_DATE, EVENT_CHRISTMAS_END_DATE);
-        dayDiscount = new DayDiscount(EVENT_START_DATE, EVENT_END_DATE);
-        presentEvent = new PresentEvent(EVENT_START_DATE, EVENT_END_DATE);
-        specialDiscount = new SpecialDiscount(EVENT_START_DATE, EVENT_END_DATE);
     }
 
     private void init() {
@@ -55,36 +39,7 @@ public class Planner {
     }
 
     public void work() {
-        organizeDiscountDetails();
-    }
-
-    private void organizeDiscountDetails() {
-        outputView.printDiscountDetails();
-
-        int date = visitDate.getDay();
-
-        int christmasDiscountAmount = christmasDiscount.calculate(date);
-        int dayDiscountAmount = dayDiscount.calculate(order, date);
-        int specialDiscountAmount = specialDiscount.calculate(date);
-        int presentEventAmount = presentEvent.calculate(order.getTotalAmount());
-
-        outputView.printChristmasDiscount(christmasDiscountAmount);
-        outputView.printDayDiscount(dayDiscountAmount);
-        outputView.printSpecialDiscount(specialDiscountAmount);
-        outputView.printPresentEvent(presentEventAmount);
-        outputView.printNewLine();
-
-        int totalDiscountAmount = christmasDiscountAmount
-                + dayDiscountAmount
-                + specialDiscountAmount
-                + presentEventAmount;
-        outputView.printTotalDiscount(totalDiscountAmount);
-        outputView.printNewLine();
-
-        int totalAmount = order.getTotalAmount();
-        int expectedAmount = totalAmount - totalDiscountAmount;
-        outputView.printExpectedAmount(expectedAmount);
-        outputView.printNewLine();
+        eventManager.organizeBenefits(order, visitDate.getDay());
     }
 
     private void informOrderMenu() {
